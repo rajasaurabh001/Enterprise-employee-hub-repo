c21a5c2c49c29d0cecfeaf3280d65aca04bcbfe1
saurabh <svikram808@gmail.com>
2026-06-28 18:09:55 +0530

    Initial Project Structur and adding 06-Information-Architecture.md 07-Component-Architecture.md and modifiy 03-Functional-Requirements.md

---
+++ spfx/src/webparts/homeDashboard/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IHomeDashboardWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'HomeDashboardWebPartStrings' {
+  const strings: IHomeDashboardWebPartStrings;
+  export = strings;
+}
